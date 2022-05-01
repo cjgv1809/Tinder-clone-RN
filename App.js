7ae567e355f9896984b7f9afe1e8508e2692057b
Carlos Gomes <carlos_gomes1809@hotmail.com>
2022-04-30 22:21:55 -0300

    add some fixes to dependencies

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./hooks/useAuth";
 import StackNavigator from "./StackNavigator";
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(["AsyncStorage", "Setting a timer"]);
 
 export default function App() {
   return (
